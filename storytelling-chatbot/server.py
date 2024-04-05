556c4b3906d565b337eac49b7124814f370eec78
Jon Taylor <jon@daily.co>
2024-04-04 17:48:22 -0700

    added basic proc mgmt

--- storytelling-chatbot/server.py
+++ storytelling-chatbot/server.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import subprocess
+import time
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -9,7 +10,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse
 
-from utils.daily_helpers import create_token
+from utils.daily_helpers import create_room as _create_room, get_token
+
+# Store the subprocesses in a dictionary
+bot_procs = {}
 
 app = FastAPI()
 
@@ -27,10 +31,7 @@ app.mount("/static", StaticFiles(directory="frontend/out",
 
 
 @app.get("/{path_name:path}", response_class=FileResponse)
-async def catch_all(path_name: str):
-    """
-    Renders our built React UI (with dynamic routing enabled)
-    """
+async def catch_all(path_name: str) -> FileResponse:
     if path_name == "":
         return FileResponse("frontend/out/index.html")
 
@@ -46,8 +47,15 @@ async def catch_all(path_name: str):
     raise HTTPException(status_code=450, detail="Incorrect API call")
 
 
+@app.post("/create")
+async def create_room(request: Request) -> JSONResponse:
+    room_url, room_name = _create_room()
+
+    return JSONResponse({"room_url": room_url, "room_name": room_name})
+
+
 @app.post("/start")
-async def start_agent(request: Request):
+async def start_agent(request: Request) -> JSONResponse:
     data = await request.json()
 
     # Is this a webhook creation request?
@@ -55,25 +63,33 @@ async def start_agent(request: Request):
         return JSONResponse({"test": True})
 
     # Ensure the room property is present
-    room_url = data.get('room')
+    room_url = data.get('room_url')
     if not room_url:
         raise HTTPException(
             status_code=500, detail="Missing 'room' property in request data. Cannot start agent without a target room!")
 
-    token = create_token(room_url)
+    token = get_token(room_url)
+
+    if not token:
+        raise HTTPException(
+            status_code=500, detail=f"Failed to get token for room: {room_url}")
 
     # Spawn a new agent, and join the user session
     # Note: this is mostly for demonstration purposes (refer to 'deployment' in README)
-    """
-    proc = subprocess.Popen(
-        [
-            f"python3 ./agent -u {room_url} -t {token}"
-        ],
-        shell=True,
-        bufsize=1,
-    )
-    """
-    return JSONResponse({"started": True})
+    try:
+        proc = subprocess.Popen(
+            [
+                f"python3 -m agent.bot -u {room_url} -t {token}"
+            ],
+            shell=True,
+            bufsize=1,
+        )
+        bot_procs[proc.pid] = proc
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Failed to start subprocess: {e}")
+
+    return JSONResponse({"started": proc.pid, "room_url": room_url})
 
 
 if __name__ == "__main__":
@@ -82,7 +98,8 @@ if __name__ == "__main__":
     default_host = os.getenv("HOST", "0.0.0.0")
     default_port = int(os.getenv("FAST_API_PORT", "7860"))
 
-    parser = argparse.ArgumentParser(description="Run the app")
+    parser = argparse.ArgumentParser(
+        description="Daily Storyteller FastAPI server")
     parser.add_argument("--host", type=str,
                         default=default_host, help="Host address")
     parser.add_argument("--port", type=int,
--- storytelling-chatbot/utils/daily_helpers.py
+++ storytelling-chatbot/utils/daily_helpers.py
@@ -12,17 +12,68 @@ daily_api_path = os.getenv("DAILY_API_URL")
 daily_api_key = os.getenv("DAILY_API_KEY")
 
 
-def create_token(room_name) -> str:
-    if not room_name:
+def create_room() -> tuple[str, str]:
+    """
+    Helper function to create a Daily room.
+    # See: https://docs.daily.co/reference/rest-api/rooms
+
+    Returns:
+        tuple: A tuple containing the room URL and room name.
+
+    Raises:
+        Exception: If the request to create the room fails or if the response does not contain the room URL or room name.
+    """
+    room_props = {
+        "exp": time.time() + 60 * 60,  # 1 hour
+        "enable_chat": True,
+        "enable_emoji_reactions": True,
+        "eject_at_room_exp": True,
+        "enable_prejoin_ui": False,  # Important for the bot to be able to join headlessly
+    }
+    res = requests.post(
+        f"https://{daily_api_path}/rooms",
+        headers={"Authorization": f"Bearer {daily_api_key}"},
+        json={
+            "properties": room_props
+        },
+    )
+    if res.status_code != 200:
+        raise Exception(f"Unable to create room: {res.text}")
+
+    data = res.json()
+    room_url: str = data.get("url")
+    room_name: str = data.get("name")
+    if room_url is None or room_name is None:
+        raise Exception("Missing room URL or room name in response")
+
+    return room_url, room_name
+
+
+def get_token(room_url: str) -> str:
+    """
+    Retrieves a meeting token for the specified Daily room URL.
+    # See: https://docs.daily.co/reference/rest-api/meeting-tokens
+
+    Args:
+        room_url (str): The URL of the Daily room.
+
+    Returns:
+        str: The meeting token.
+
+    Raises:
+        Exception: If no room URL is specified or if no Daily API key is specified.
+        Exception: If there is an error creating the meeting token.
+    """
+    if not room_url:
         raise Exception(
-            "No Daily room specified. use the -u/--url option from the command line, or set DAILY_SAMPLE_ROOM_URL in your environment to specify a Daily room URL.")
+            "No Daily room specified. You must specify a Daily room in order a token to be generated.")
 
     if not daily_api_key:
         raise Exception(
             "No Daily API key specified. set DAILY_API_KEY in your environment to specify a Daily API key, available from https://dashboard.daily.co/developers.")
 
-    # room_name: str = urllib.parse.urlparse(url).path[1:]
     expiration: float = time.time() + 60 * 60
+    room_name = urllib.parse.urlparse(room_url).path[1:]
 
     res: requests.Response = requests.post(
         f"https://{daily_api_path}/meeting-tokens",
